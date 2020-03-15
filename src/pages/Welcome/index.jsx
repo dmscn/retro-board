6a28468a7029fa761e37776b6bbb6bb5f44f3a2c
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2020-03-15 12:54:32 -0300

    feat(Welcome): consuming store

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import { useStore } from '@store'
+import { IncrementCounter, DecrementCounter } from '@store/Counter'
 
 export default function Welcome() {
-  return <div>Welcome Page</div>
+  const {
+    store: { counter },
+    dispatch,
+  } = useStore()
+
+  const dispatchCounterIncrement = () => dispatch(IncrementCounter())
+  const dispatchCounterDecrement = () => dispatch(DecrementCounter())
+
+  return (
+    <div>
+      Welcome Page, you counter is on {counter}
+      <button onClick={dispatchCounterIncrement}>(+) Increment</button>
+      <button onClick={dispatchCounterDecrement}>(-) Decrement</button>
+    </div>
+  )
 }
