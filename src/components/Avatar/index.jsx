463dbc9f898fbd186a7c0d7fca5051d831bfdb50
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-31 15:30:30 -0300

    feat(Components): Avatar

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { theme } from '@gympass/yoga'
+
+export default styled.img`
+  display: flex;
+  height: ${theme.spacing.xxlarge}px;
+  width: ${theme.spacing.xxlarge}px;
+  border: 2px solid ${theme.colors.white};
+  border-radius: 100%;
+`
