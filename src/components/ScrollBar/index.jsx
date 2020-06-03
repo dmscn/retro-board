552b8c324aeb2b2391e8e30eca71361d284ee139
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-06-03 20:47:23 -0300

    feat(Components): Scrollbar

---
+++ src/components/ScrollBar/index.jsx
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components'
+
+const ScrollBar = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  ${({ vertical, horizontal, both }) => css`
+    overflow-y: ${(both || vertical) && 'auto'};
+    overflow-x: ${(both || horizontal) && 'auto'};
+  `}
+`
+
+export default ScrollBar
