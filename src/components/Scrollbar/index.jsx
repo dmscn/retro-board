3c3464c173202731bf920b04de0bc7b5cdd17119
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-06-09 19:54:49 -0300

    style(Scrollbar): Custom scrollbar

--- src/components/Scrollbar/index.jsx
+++ src/components/Scrollbar/index.jsx
@@ -1,6 +1,26 @@
 import styled, { css } from 'styled-components'
+import hexToRgb from '@utils/hexToRgb'
+import { theme } from '@gympass/yoga'
+
+const customScrollbarCss = css`
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px
+      ${props => hexToRgb(theme.colors.dark(props), 0.5)};
+    background-color: ${theme.colors.white};
+  }
+
+  ::-webkit-scrollbar {
+    width: 6px;
+    background-color: ${theme.colors.white};
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${theme.colors.primary[1]};
+  }
+`
 
 const Scrollbar = styled.div`
+  ${customScrollbarCss};
   box-sizing: border-box;
   height: 100%;
   width: 100%;
