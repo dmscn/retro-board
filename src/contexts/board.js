74e4f53a5fb71fb827c69f23b4cf0da0eb687ed9
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-06-20 16:39:50 -0300

    feat(Board context): firebase in custom hook

--- src/contexts/board.js
+++ src/contexts/board.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import {
+  subscribeBoardColumns,
+  addNewColumnToBoard,
+  removeColumnFromBoard,
+} from '@services/firebase'
 
 const BoardContext = React.createContext()
 
-export const BoardProvider = ({ children, value }) => (
-  <BoardContext.Provider value={value}>{children}</BoardContext.Provider>
-)
+export const BoardProvider = ({ children, slug }) => {
+  const [columns, setColumns] = React.useState([])
+
+  React.useEffect(() => {
+    subscribeBoardColumns(slug, setColumns)
+  }, [])
+
+  const addColumn = title => addNewColumnToBoard(slug, { title })
+  const removeColumn = columnSlug => removeColumnFromBoard(slug, columnSlug)
+
+  const contextValues = {
+    slug,
+    columns,
+    addColumn,
+    removeColumn,
+  }
+
+  return (
+    <BoardContext.Provider value={contextValues}>
+      {children}
+    </BoardContext.Provider>
+  )
+}
 
 export const useBoard = () => React.useContext(BoardContext)
