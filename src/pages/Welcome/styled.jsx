264facd1075667f89b2530543de6c1de8e27a764
Leonardo Damasceno <leonardo.damasceno@gympass.com>
2021-11-25 11:30:47 -0300

    fix: mobile responsiviness for welcome page

--- src/pages/Welcome/styled.jsx
+++ src/pages/Welcome/styled.jsx
@@ -1,36 +1,5 @@
 import styled from 'styled-components'
-import { Row, Button, theme } from '@gympass/yoga'
-
-export const ViewportLane = styled.main`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  ${({
-    theme: {
-      yoga: { colors },
-    },
-  }) => `
-    background: ${colors.energy[0]}
-  `}
-`
-export const CenteredRow = styled(Row)`
-  display: flex;
-  justify-content: center;
-  margin: 16px 0;
-  text-align: center;
-`
-export const IllustrationWrapper = styled(CenteredRow)`
-  margin: 48px 0;
-`
-
-export const ButtonsRow = styled(CenteredRow)`
-  .with-margin {
-    margin-right: ${theme.spacing.medium}px;
-  }
-`
+import { Button, theme } from '@gympass/yoga'
 
 export const FixedWidthButton = styled(Button.Outline)`
   width: calc(${theme.spacing.huge}px * 2);
